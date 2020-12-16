14dd52ae2398a2b9ec7b299dc91f3a6a345be6bf
robot1525 <73985895+robot1525@users.noreply.github.com>
2020-12-16 13:45:11 +0800

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,7 +1,15 @@
+echo "/proc/cpuinfo: "
+echo -e "\n"
 cat /proc/cpuinfo
+echo -e "\n"
+echo "cat /proc/meminfo: "
 echo -e "\n"
 cat /proc/meminfo
 echo -e "\n"
+echo "df -h: "
+echo -e "\n"
 df -h
+echo -e "\n"
+echo "free -m: "
 echo -e "\n"
 free -m
